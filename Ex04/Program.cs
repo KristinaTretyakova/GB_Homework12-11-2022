d416585026b6cd6cbf11f53689e92914cbab31fa
Kristina <kristina.yakimova.96@gmail.com>
2022-11-12 21:46:13 +0200

    add ex04

---
+++ Ex04/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу, которая на вход принимает число (N),
+ а на выходе показывает все чётные числа от 1 до N.*/
+
+int numberA = 1; //Начало диапазона;
+
+Console.Write("Введите максимальное число диапазона ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int a = numberA;
+
+while ( a <= numberB)
+{
+    if ( a % 2 == 0)
+    {
+    Console.Write( a + " ");
+    }
+    a++;
+}
