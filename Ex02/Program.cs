f613fc653c33c1ac76e1fef8e5f3dd88b1e56188
Kristina <kristina.yakimova.96@gmail.com>
2022-11-12 21:45:53 +0200

    add ex02

---
+++ Ex02/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая принимает на вход три числа
+ и выдаёт максимальное из этих чисел*/
+
+Console.Write("Введите первое число ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число ");
+int numberС = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+if (numberB > max) max = numberB;
+if (numberС > max) max = numberС;
+
+Console.Write("Максимальное число ");
+Console.WriteLine(max);
