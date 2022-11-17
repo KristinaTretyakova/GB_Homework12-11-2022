066eae8a8c51afe1fa60c74abce1747633db6ab0
Kristina <kristina.yakimova.96@gmail.com>
2022-11-17 18:12:29 +0200

    add Ex06

---
+++ Ex06/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу, которая выводит третью цифру 
+заданного числа или сообщает, что третьей цифры нет.*/
+
+Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while (number > 1000)
+{
+    number = number / 10;
+}
+
+int threenumber = (number % 10);
+
+if( number > 99 && number < 1000)
+{
+    Console.WriteLine($"Третья цифра этого числа равна {threenumber}");
+}
+else
+{
+    Console.WriteLine(" Третьего числа нет:)");
+}
