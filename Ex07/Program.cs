cd62dd0b354dd27207b2fb5502d94d98628c3ec5
Kristina <kristina.yakimova.96@gmail.com>
2022-11-17 18:42:10 +0200

    add Ex07

---
+++ Ex07/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.*/
+
+Console.Write("Введите число от 1 до 7  ");
+int dayweek = Convert.ToInt32(Console.ReadLine());
+
+if(dayweek > 0 && dayweek < 6)
+{
+    Console.WriteLine("К сожалению не выходной:(");
+}
+else if (dayweek > 5 && dayweek < 8)
+{
+    Console.WriteLine("Выходной:)");
+}
+else
+{
+    Console.WriteLine("Введите корректное число");
+}
