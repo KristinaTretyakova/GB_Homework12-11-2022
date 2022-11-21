ac1e5564f8bc32d58008d0607b9e9d6b3091f4ab
Kristina <kristina.yakimova.96@gmail.com>
2022-11-21 16:16:09 +0200

    add Ex08

---
+++ Ex08/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное 
+число и проверяет, является ли оно палиндромом.*/
+
+int number = ReadInt("Введите число для проверки: ");
+int temp = number;
+int unnumber = 0;
+
+while (temp > 0)
+    {
+        unnumber = unnumber * 10;
+        unnumber = unnumber + temp % 10;
+        temp = temp  / 10;
+    }
+
+if (number == unnumber)
+{ 
+    Console.WriteLine("Введенное Вами число является палиндромом");
+}
+else
+{
+    Console.WriteLine("Введенное Вами число НЕ является палиндромом");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
