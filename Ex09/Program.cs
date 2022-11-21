c80cf8543167b946433f30767ff952d4b5892165
Kristina <kristina.yakimova.96@gmail.com>
2022-11-21 16:39:22 +0200

    add Ex09

---
+++ Ex09/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.*/
+
+int aX = ReadInt("Введите координату Х точки А: ");
+int aY = ReadInt("Введите координату Y точки А: ");
+int aZ = ReadInt("Введите координату Z точки А: ");
+int bX = ReadInt("Введите координату Х точки B: ");
+int bY = ReadInt("Введите координату Y точки B: ");
+int bZ = ReadInt("Введите координату Z точки B: ");
+
+int sqrSideX = (aX - bX) * (aX - bX);
+int sqrSideY = (aY - bY) * (aY - bY);
+int sqrSideZ = (aZ - bZ) * (aZ - bZ);
+
+double distance = Math.Sqrt(sqrSideX + sqrSideY +sqrSideZ);
+Console.WriteLine("Растояние между точками равно :" + distance);
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
