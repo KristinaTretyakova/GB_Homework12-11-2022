62a44eed9b88feec76a56771d432886c426f819f
Kristina <kristina.yakimova.96@gmail.com>
2022-11-25 20:11:52 +0200

    add Ex11

---
+++ Ex11/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.*/
+
+int numberA = ReadInt("Введите число A: ");
+int numberB = ReadInt("Введите число B: ");
+int result = 1;
+
+for ( int i = 0; i< numberB; i++)
+
+{
+    result *=  numberA;
+}
+
+Console.WriteLine($"Число {numberA} в степени {numberB} равно = {result}");
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
