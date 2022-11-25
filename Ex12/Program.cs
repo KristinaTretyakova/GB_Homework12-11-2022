a8a8e06298bf64330ce271d3ba5d5c6362280b38
Kristina <kristina.yakimova.96@gmail.com>
2022-11-25 20:31:55 +0200

    add Ex12

---
+++ Ex12/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.*/
+
+int number = ReadInt("Введите число: ");
+int sum = 0;
+
+while (number!=0)
+{
+    int temp = number % 10;
+    number = number / 10;
+    sum = sum + temp;
+}
+
+Console.WriteLine($"Сумма чисел введенного Вами числа равна {sum}");
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
