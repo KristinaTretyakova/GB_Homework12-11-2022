2cffa026df6402e76525ff389716bc81a8c8a063
Kristina <kristina.yakimova.96@gmail.com>
2022-11-27 17:37:11 +0200

    add Ex14

---
+++ Ex14/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.*/
+
+int size = ReadInt("Введите размер массива: ");
+int[] numbers = new int[size];
+int count = 0;
+FillArrayRandomNumbers(numbers, 99, 1000);
+WriteArray(numbers);
+
+for(int i = 0; i < numbers.Length; i++)
+{
+    if ( numbers[i] % 2 == 0)
+    {
+        count = count + 1;
+    }
+}
+
+Console.WriteLine(count);
+
+void FillArrayRandomNumbers(int[] array, int min, int max)
+{
+    for(int i = 0; i < array.Length; i++)
+        {
+            array[i] = new Random().Next(min, max + 1);
+        }
+}
+
+void WriteArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        {
+        Console.Write(array[i] + " ");
+        }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+    {      Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+    }
