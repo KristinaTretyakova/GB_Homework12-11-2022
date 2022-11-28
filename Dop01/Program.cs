aea0e6cc65e92b798f1ccc18f50d0a0410987439
Kristina <kristina.yakimova.96@gmail.com>
2022-11-28 20:07:39 +0200

    add dop01

---
+++ Dop01/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Программа с бесконечным циклом. Как только пользователь введет Exit программа остановится. */ 
+
+Console.WriteLine("Нажмите Enter для запуска программы:)");
+        string? Enter = Console.ReadLine();
+
+Method(Enter);
+
+void Method(string? text)   
+    {
+        Console.WriteLine("Напишите Exit для остановки программы... ");
+        string? input = Console.ReadLine();
+        if (input == "Exit") return;
+        else Method(Enter);
+    }   
+
