01641066dc719a0420b4ce4c3965ea34298131d4
Kristina <kristina.yakimova.96@gmail.com>
2022-12-10 16:42:40 +0200

    add ex27

---
+++ Ex27/Program.cs
@@ -0,0 +1,32 @@
+﻿/*  Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии. */
+
+int n = ReadInt("Введите число N: ");
+int end = 1;
+
+NaturalNumber(n,end);
+
+void NaturalNumber(int a, int b)
+
+{
+    if (a == b)
+    {
+        Console.Write($"{a}");
+    }
+    if (a<b)
+    {
+        Console.Write($"{a}, ");
+        NaturalNumber(a+1,b);
+    }
+    if (a>b)
+    {
+        Console.Write($"{a}, ");
+        NaturalNumber(a-1,b);
+    }
+}
+
+int ReadInt(string message)
+    {
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+    }
